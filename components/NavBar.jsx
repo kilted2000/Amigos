048a03c7824bdd2562c49addb26f6c3d1c4f36f5
Richard <richardlwhittington@gmail.com>
2023-06-28 23:33:49 -0500

    add links

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -12,24 +12,11 @@ const NavBar = () => {
   return (
  <>
     <div className="header" >
-      <div className='navbar navbar-expand-lgbar-dark nav' id="title">
-      
-    
-        <div className='.container-fluid '>
+  <div id="title">
+        <div className='container-fluid '>
         <nav className="navbar navbar-expand-lgbar-dark nav">
-            <a className="navbar-brand"></a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarTogglerDemo01"
-              aria-controls="navbarTogglerDemo01"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            
+            {/* <div className="collapse navbar-collapse" id="navbarTogglerDemo01"> */}
               <ul className="navbar-nav ms-auto">
                 <li className="navbar-item">
                   <a className="nav-link" href="#footer">Contact</a>
@@ -41,10 +28,10 @@ const NavBar = () => {
                   <Link className="nav-link" href="#cta">Sign In</Link>
                 </li>
               </ul>
-            </div>
+            {/* </div> */}
           </nav>
       </div>
-    </div>
+      </div> 
     </div>
     
   </>
