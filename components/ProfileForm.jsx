dcc845cd576624461800ba0e666b7bb18427e5ac
Richard <richardlwhittington@gmail.com>
2023-07-27 16:55:33 -0500

    add upload widget

--- components/ProfileForm.jsx
+++ components/ProfileForm.jsx
@@ -1,6 +1,8 @@
 // components/ProfileForm.js
-'use client'
+'use client';
 import { useState, useEffect } from 'react';
+
+import { CldImage, CldUploadWidget } from 'next-cloudinary';
 //intigrate MongoDB here
 
 const ProfileForm = () => {
@@ -12,128 +14,151 @@ const ProfileForm = () => {
 
   useEffect(() => {
     fetch('/api/profile')
-      .then((response) => response.json())
-      .then((data) => {
+      .then(response => response.json())
+      .then(data => {
         setFirstName(data.firstName);
         setLastName(data.lastName);
         setDogName(data.dogName);
         setBreed(data.breed);
         setPersonality(data.personality);
       })
-      .catch((error) => console.error(error));
+      .catch(error => console.error(error));
   }, []);
 
-  const handleSubmit = (event) => {
+function handlePhotoUpload(result){
+console.log(result);
+}
+
+  const handleSubmit = event => {
     event.preventDefault();
-    const updatedProfile = { firstName,lastName, dogName, breed, personality};
+    const updatedProfile = { firstName, lastName, dogName, breed, personality };
 
     fetch('/api/profile', {
       method: 'PUT',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(updatedProfile),
+      body: JSON.stringify(updatedProfile)
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error(error));
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
   };
-  
+
   return (
     <form onSubmit={handleSubmit}>
       <label>
         First Name:
-        <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+        <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)} />
       </label>
       <label>
         Last Name:
-        <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+        <input type="text" value={lastName} onChange={e => setLastName(e.target.value)} />
       </label>
       <label>
         Dog's Name:
-        <input type="text" value={dogName} onChange={(e) => setDogName(e.target.value)} />
+        <input type="text" value={dogName} onChange={e => setDogName(e.target.value)} />
       </label>
       <label>
         Breed:
-        <input value={breed} onChange={(e) => setBreed(e.target.value)} />
+        <input value={breed} onChange={e => setBreed(e.target.value)} />
       </label>
       <label>
         Doggie Personality:
-        <textarea value={personality} onChange={(e) => setPersonality(e.target.value)} />
+        <textarea value={personality} onChange={e => setPersonality(e.target.value)} />
       </label>
       <label>
         Photos of Doggie:
-        <textarea value={personality} onChange={(e) => setPersonality(e.target.value)} />
+        <CldUploadWidget uploadPreset="barkamigos-dog-photos" onUpload={handlePhotoUpload}>
+          {({ open }) => {
+            function handleOnClick(e) {
+              e.preventDefault();
+              open();
+            }
+            return (
+              <button className="button" onClick={handleOnClick}>
+                Upload an Image
+              </button>
+            );
+          }}
+        </CldUploadWidget>
       </label>
       <div className="form-check">
-      <input className="form-check-input " type="checkbox" value="" id="invalidCheck3" aria-describedby="invalidCheck3Feedback" required />
-       <label className="form-check-label" for="invalidCheck3">
-         Agree to terms and conditions
-       </label>
-     <div id="invalidCheck3Feedback" class="invalid-feedback">
-        You must agree before submitting.
+        <input
+          className="form-check-input "
+          type="checkbox"
+          value=""
+          id="invalidCheck3"
+          aria-describedby="invalidCheck3Feedback"
+          required
+        />
+        <label className="form-check-label" for="invalidCheck3">
+          Agree to terms and conditions
+        </label>
+        <div id="invalidCheck3Feedback" class="invalid-feedback">
+          You must agree before submitting.
+        </div>
       </div>
-    </div>
 
       <button type="submit">Save Profile</button>
     </form>
-//     <form className="row g-3">
-//   <div className="col-md-4">
-//     <label for="validationServer01" className="form-label">First name</label>
-//     <input type="text" className="form-control is-valid" id="validationServer01" value="Mark" required />
-//     <div className="valid-feedback">
-//       Looks good!
-//     </div>
-//   </div>
-//   <div className="col-md-4">
-//     <label for="validationServer02" class="form-label">Last name</label>
-//     <input type="text" className="form-control is-valid" id="validationServer02" value="Otto" required />
-//     <div className="valid-feedback">
-//       Looks good!
-//     </div>
-//   </div>
-//   <div className="col-md-6">
-//     <label for="validationServer03" className="form-label">City</label>
-//     <input type="text" className="form-control is-invalid" id="validationServer03" aria-describedby="validationServer03Feedback" required />
-//     <div id="validationServer03Feedback" className="invalid-feedback">
-//       Please provide a valid city.
-//     </div>
-//   </div>
-//   <div className="col-md-3">
-//     <label for="validationServer04" className="form-label">State</label>
-//     <select className="form-select is-invalid" id="validationServer04" aria-describedby="validationServer04Feedback" required>
-//       <option selected disabled value="">Choose...</option>
-//       <option>Tennessee</option>
-//       <option>New York</option>
-//       <option>Washington</option>
-//       <option>Mississippi</option>
-//     </select>
-//     <div id="validationServer04Feedback" className="invalid-feedback">
-//       Please select a valid state.
-//     </div>
-//   </div>
-//   <div className="col-md-3">
-//     <label for="validationServer05" className="form-label">Zip</label>
-//     <input type="text" className="form-control is-invalid" id="validationServer05" aria-describedby="validationServer05Feedback" required />
-//     <div id="validationServer05Feedback" className="invalid-feedback">
-//       Please provide a valid zip.
-//     </div>
-//   </div>
-//   <div className="col-12">
-//     <div className="form-check">
-//       <input class="form-check-input is-invalid" type="checkbox" value="" id="invalidCheck3" aria-describedby="invalidCheck3Feedback" required />
-//       <label className="form-check-label" for="invalidCheck3">
-//         Agree to terms and conditions
-//       </label>
-//       <div id="invalidCheck3Feedback" class="invalid-feedback">
-//         You must agree before submitting.
-//       </div>
-//     </div>
-//   </div>
-//   <div className="col-12">
-//     <button className="btn btn-primary" type="submit">Submit form</button>
-//   </div>
-// </form>
+    //     <form className="row g-3">
+    //   <div className="col-md-4">
+    //     <label for="validationServer01" className="form-label">First name</label>
+    //     <input type="text" className="form-control is-valid" id="validationServer01" value="Mark" required />
+    //     <div className="valid-feedback">
+    //       Looks good!
+    //     </div>
+    //   </div>
+    //   <div className="col-md-4">
+    //     <label for="validationServer02" class="form-label">Last name</label>
+    //     <input type="text" className="form-control is-valid" id="validationServer02" value="Otto" required />
+    //     <div className="valid-feedback">
+    //       Looks good!
+    //     </div>
+    //   </div>
+    //   <div className="col-md-6">
+    //     <label for="validationServer03" className="form-label">City</label>
+    //     <input type="text" className="form-control is-invalid" id="validationServer03" aria-describedby="validationServer03Feedback" required />
+    //     <div id="validationServer03Feedback" className="invalid-feedback">
+    //       Please provide a valid city.
+    //     </div>
+    //   </div>
+    //   <div className="col-md-3">
+    //     <label for="validationServer04" className="form-label">State</label>
+    //     <select className="form-select is-invalid" id="validationServer04" aria-describedby="validationServer04Feedback" required>
+    //       <option selected disabled value="">Choose...</option>
+    //       <option>Tennessee</option>
+    //       <option>New York</option>
+    //       <option>Washington</option>
+    //       <option>Mississippi</option>
+    //     </select>
+    //     <div id="validationServer04Feedback" className="invalid-feedback">
+    //       Please select a valid state.
+    //     </div>
+    //   </div>
+    //   <div className="col-md-3">
+    //     <label for="validationServer05" className="form-label">Zip</label>
+    //     <input type="text" className="form-control is-invalid" id="validationServer05" aria-describedby="validationServer05Feedback" required />
+    //     <div id="validationServer05Feedback" className="invalid-feedback">
+    //       Please provide a valid zip.
+    //     </div>
+    //   </div>
+    //   <div className="col-12">
+    //     <div className="form-check">
+    //       <input class="form-check-input is-invalid" type="checkbox" value="" id="invalidCheck3" aria-describedby="invalidCheck3Feedback" required />
+    //       <label className="form-check-label" for="invalidCheck3">
+    //         Agree to terms and conditions
+    //       </label>
+    //       <div id="invalidCheck3Feedback" class="invalid-feedback">
+    //         You must agree before submitting.
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <div className="col-12">
+    //     <button className="btn btn-primary" type="submit">Submit form</button>
+    //   </div>
+    // </form>
   );
 };
 
--- pages/api/profile.js
+++ pages/api/profile.js
@@ -8,18 +8,18 @@
 
  
 
-  export default async function handler(req, res) {
-    if (req.method === 'GET') {
-      // Retrieve profile data
-      //res.status(200).json(profileData);
-      await dbConnect();
-    } else if (req.method === 'PUT') {
-      // Update profile data
-      profileData = req.body;
-      res.status(200).json({ message: 'Profile updated successfully.' });
-    } else {
-      res.status(405).json({ message: 'Method not allowed.' });
-    }
+  // export default async function handler(req, res) {
+  //   if (req.method === 'GET') {
+  //     // Retrieve profile data
+  //     //res.status(200).json(profileData);
+  //     await dbConnect();
+  //   } else if (req.method === 'PUT') {
+  //     // Update profile data
+  //     profileData = req.body;
+  //     res.status(200).json({ message: 'Profile updated successfully.' });
+  //   } else {
+  //     res.status(405).json({ message: 'Method not allowed.' });
+  //   }
     
-  }
+  // }
   
