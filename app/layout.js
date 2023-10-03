4e7b8a50641cbaac9d412a40012221c65be530d2
Richard <richardlwhittington@gmail.com>
2023-10-03 14:16:59 -0500

    add app

---
+++ app/layout.js
@@ -0,0 +1,39 @@
+export default function RootLayout({
+    // Layouts must accept a children prop.
+    // This will be populated with nested layouts or pages
+    children,
+  }) {
+    return (
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    )
+  }
+
+
+  export const metadata = {
+    title: 'Home',
+    description: 'Welcome to Next.js',
+  }
+
+  import React from 'react';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+
+import Layout from '../components/Layout';
+
+//import '@fortawesome/fontawesome-svg-core/styles.css';
+
+import '../styles/globals.css';
+
+
+//initFontAwesome();
+
+export default function App({ Component, pageProps }) {
+  return (
+    <UserProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </UserProvider>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,20 @@
+"use client";
+import React from 'react';
+
+import API from '../api/api.js/api'
+import Content from '../components/Content';
+import Pricing from '../components/pricing';
+import Cta from '../components/Cta';
+import Hero from '../components/Hero';
+export default function Index() {
+  return (
+    <>
+      
+     <Hero />
+      <Content />
+      
+      <Pricing />
+      <Cta />
+    </>
+  );
+}
